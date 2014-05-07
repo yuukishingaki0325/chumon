d7ceccf8f83db018a6496c8a792463f0baf239de
Yuuki SHINGAKI <rurero@example.com>
2014-05-07 09:43:01 +0900

    Start "chumon" Project!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chumon::Application.config.secret_key_base = '611b0836e04d133db871655efb9beb61df0703b10b0c1a3a7c10b56a584a54ac5a4957e7f4f219fc12b43dd3bee83a1d27900c5eb83920bbccf4f0e4d2914c5a'
